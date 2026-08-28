201e95efaec678da60c13a54498528892f1e67b4
Your Name <you@example.com>
2026-08-28 13:51:40 +0000

    changed approach

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "casestudy-terraform-state-569103037896"
-    key          = "apprunner/terraform.tfstate"
+    key          = "private-ec2/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,26 +4,44 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the private subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c101f26f147fa7fd"
+}
+
 variable "ecr_repository_name" {
   description = "Private ECR repository name"
   type        = string
   default     = "my-nginx"
 }
 
-variable "app_runner_service_name" {
-  description = "App Runner service name"
-  type        = string
-  default     = "my-nginx-service"
-}
-
 variable "image_tag" {
   description = "Docker image tag stored in ECR"
   type        = string
   default     = "latest"
-}
-
-variable "deploy_app_runner" {
-  description = "Whether to deploy the App Runner service"
-  type        = bool
-  default     = false
 }
