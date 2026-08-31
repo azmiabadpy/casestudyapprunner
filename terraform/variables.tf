ba2c0c5638263e8879b5714f2f15483880f1ba10
Your Name <you@example.com>
2026-08-31 08:55:06 +0000

    changed the ami

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,7 +31,7 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-01a00762f46d584a1"
+  default     = "ami-0b6d9d3d33ba97d99"
 }
 
 variable "ecr_repository_name" {
